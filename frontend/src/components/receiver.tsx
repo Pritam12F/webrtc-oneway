4adc92a457ed2b18b18be24bde36263d430e9f52
Pritam12F <pritam.das.santu@gmail.com>
2024-08-20 14:08:48 +0530

    added connection to websocket backend

--- frontend/src/components/receiver.tsx
+++ frontend/src/components/receiver.tsx
@@ -1,3 +1,15 @@
+import { useEffect, useState } from "react";
+
 export const Receiver = () => {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+    setSocket(socket);
+    socket.onopen = () => {
+      socket.send(JSON.stringify({ type: "receiver" }));
+    };
+  }, []);
+
   return <div>Receiver message</div>;
 };
--- frontend/src/components/sender.tsx
+++ frontend/src/components/sender.tsx
@@ -1,3 +1,26 @@
+import { useEffect, useState } from "react";
+
 export const Sender = () => {
-  return <div>Sender message</div>;
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [pc, setPC] = useState<RTCPeerConnection | null>(null);
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+    setSocket(socket);
+    socket.onopen = () => {
+      socket.send(JSON.stringify({ type: "sender" }));
+    };
+  }, []);
+
+  const initiateConnection = async () => {
+    if (!socket) {
+      alert("Socket not found!");
+      return;
+    }
+
+    const pc = new RTCPeerConnection();
+    setPC(pc);
+  };
+
+  return <button onClick={initiateConnection}>Click me</button>;
 };
