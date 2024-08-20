3b6417c3fe10865becf667607b09a963e8a53440
Pritam12F <pritam.das.santu@gmail.com>
2024-08-20 13:02:30 +0530

    backend completed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 
 let senderSocket: null | WebSocket;
 let receiverSocket: null | WebSocket;
@@ -12,6 +12,50 @@ wss.on("connection", function (ws) {
 
   ws.on("message", function (data: any) {
     const message = JSON.parse(data);
+
+    if (message.type === "sender") {
+      if (!senderSocket) {
+        senderSocket = ws;
+      }
+    } else if (message.type === "receiever") {
+      if (!receiverSocket) {
+        receiverSocket = ws;
+      }
+    } else if (message.type === "createOffer") {
+      if (ws !== senderSocket) {
+        return;
+      }
+
+      receiverSocket?.send(
+        JSON.stringify({ type: "createOffer", sdp: message.sdp })
+      );
+    } else if (message.type === "createAnswer") {
+      if (ws !== receiverSocket) {
+        return;
+      }
+
+      senderSocket?.send(
+        JSON.stringify({ type: "createAnswer", sdp: message.sdp })
+      );
+    } else if (message.type === "iceCanditates") {
+      if (ws === senderSocket) {
+        receiverSocket?.send(
+          JSON.stringify({
+            type: "iceCanditates",
+            iceType: "senderIce",
+            iceCandidates: message.iceCandidates,
+          })
+        );
+      } else if (ws === receiverSocket) {
+        senderSocket?.send(
+          JSON.stringify({
+            type: "iceCanditates",
+            iceType: "receiverIce",
+            iceCandidates: message.iceCandidates,
+          })
+        );
+      }
+    }
   });
 
   ws.send("Connected!");
